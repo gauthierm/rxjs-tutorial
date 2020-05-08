1025a56cf1976f3e1ba7c037d44ca2c23366f1ac
Michael Gauthier <mike@silverorange.com>
2020-05-08 15:37:25 -0300

    Move error simulator into terrible middleware

---
+++ api/src/middleware/terrible.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from 'express';
+export function terrible(
+  failureRate = 0.5,
+  minimumDelayMs = 1000,
+  maximumDelayMs = 6000
+) {
+  return function terribleMiddleware(
+    _: Request,
+    __: Response,
+    next: NextFunction
+  ) {
+    const delayMs = Math.floor(
+      Math.random() * (maximumDelayMs - minimumDelayMs) + minimumDelayMs
+    );
+
+    setTimeout(() => {
+      if (Math.random() < failureRate) {
+        next({
+          status: 400,
+          message: 'A wild error appeared.',
+        });
+      } else {
+        next();
+      }
+    }, delayMs);
+  };
+}
