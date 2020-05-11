53b7a499959a1f6d4abc4726d72df8538761b2a2
Michael Gauthier <mike@silverorange.com>
2020-05-11 02:54:45 -0300

    Add individual post route and remove post body from index

--- api/src/routes/posts.ts
+++ api/src/routes/posts.ts
@@ -2,14 +2,38 @@ import { Router, Request, Response } from 'express';
 import { posts as postsData } from '../data/posts';
 import shuffle from 'knuth-shuffle-seeded';
 import { terrible } from '../middleware/terrible';
+import { debug as createDebug } from 'debug';
+
+const debug = createDebug('api');
 
 export const posts = Router();
 
 posts.get('/', terrible(), async (_: Request, res: Response) => {
+  debug('Getting posts index.');
+
   shuffle(postsData);
 
-  res.header('Content-Type', 'application/json');
+  const postIndexData = postsData.map((post) => ({
+    id: post.id,
+    title: post.title,
+    author: post.author,
+    publishedAt: post.publishedAt,
+  }));
+
   res.header('Cache-Control', 'no-store');
   res.status(200);
-  res.json(postsData);
+  res.json(postIndexData);
+});
+
+posts.get('/:id', terrible(), async (req: Request, res: Response) => {
+  debug(`Getting post ${req.params.id}.`);
+
+  const post = postsData.filter((post) => post.id === req.params.id)[0];
+  if (post === null) {
+    res.status(404);
+    res.json({ message: 'Not found.' });
+  } else {
+    res.status(200);
+    res.json(post);
+  }
 });
